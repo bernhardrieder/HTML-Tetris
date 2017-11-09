cabaa15636628566bed1002a02ddaba9df0788ea
Bernhard Rieder <bernhard.rieder@live.at>
2017-11-09 14:57:49 +0100

    added jquery; refactored row clear; refactored field/grid

--- wwwroot/tetris.js
+++ wwwroot/tetris.js
@@ -13,39 +13,45 @@
 
 /* ~~~~~~ actual game ~~~~~~ */
 
-var fieldSize = {
-    x: 10,
-    y: 20
-}
+var grid = {
+    size: { x: 10, y: 20 },
+    matrix: [],
 
-var fieldMatrix = [];
-for (var x = 0; x < fieldSize.x; ++x) {
-    if (!fieldMatrix[x])
-        fieldMatrix[x] = [];
-    for (var y = 0; y < fieldSize.y; ++y)
-        fieldMatrix[x][y] = 0;
+    init: function() {
+        for (var x = 0; x < this.size.x; ++x) {
+            this.matrix[x] = [];
+            for (var y = 0; y < this.size.y; ++y)
+                this.matrix[x][y] = undefined;
+        }
+    },
+
+    moveBlock : function(block) {
+        return true;
+    }
 }
 
 var dirtyRectangles = [];
 var blocksOnField = [];
 var backgroundCanvas = document.getElementById("background");
 var backgroundContext = backgroundCanvas.getContext("2d");
-var fieldCanvas = document.getElementById("main"); 
+var fieldCanvas = document.getElementById("main");
 var fieldCanvasContext = fieldCanvas.getContext("2d");
 var lastLoopTime = 0;
 var isGameOver = false;
 
 function init() {
+    grid.init();
+
     //setup background canvas
     backgroundCanvas.width = window.innerWidth;
     backgroundCanvas.height = window.innerHeight;
-   
+
     //setup main canvas
     fieldCanvas.width = window.innerWidth;
     fieldCanvas.height = window.innerHeight;
 
     //setup game loop
-    var loop = function (time = 0) {
+    var loop = function(time = 0) {
         var deltaTime = time - lastLoopTime;
         lastLoopTime = time;
         update(deltaTime);
@@ -56,9 +62,9 @@ function init() {
     window.requestAnimationFrame(loop, backgroundCanvas);
 
     //setup walls
-    for (var x = 0; x <= fieldSize.x + 1; ++x) {
-        for (var y = 0; y <= fieldSize.y; ++y) {
-            if (x === 0 || x === fieldSize.x + 1 || y === fieldSize.y) {
+    for (var x = 0; x <= grid.size.x + 1; ++x) {
+        for (var y = 0; y <= grid.size.y; ++y) {
+            if (x === 0 || x === grid.size.x + 1 || y === grid.size.y) {
                 new Block(y % 2 === 0 ? "grey" : "black", x * blockSize, y * blockSize).draw(backgroundContext);
             }
         }
@@ -77,7 +83,7 @@ function Block(color, x, y) {
     this.y = y;
     this.dirty = true;
 
-    this.draw = function (canvasContext) {
+    this.draw = function(canvasContext) {
         canvasContext.fillStyle = color;
         canvasContext.fillRect(this.x, this.y, this.width, this.height);
         this.dirty = false;
@@ -94,12 +100,12 @@ function Block(color, x, y) {
 }
 
 function FieldBlock(color, fieldX, fieldY) {
-    if (fieldMatrix[fieldX][fieldY] === 1) {
+    if (grid.matrix[fieldX][fieldY]) {
         gameOver();
         return;
     }
 
-    this.getCanvasPositionFromFieldPosition = function (x, y) {
+    this.getCanvasPositionFromFieldPosition = function(x, y) {
         return {
             x: blockSize + x * blockSize,
             y: y * blockSize
@@ -109,20 +115,21 @@ function FieldBlock(color, fieldX, fieldY) {
     var pos = this.getCanvasPositionFromFieldPosition(fieldX, fieldY);
     Block.call(this, color, pos.x, pos.y);
 
-    this.fieldPosition = { x: fieldX, y: fieldY}; 
-    fieldMatrix[fieldX][fieldY] = 1;
+    this.fieldPosition = { x: fieldX, y: fieldY };
+    grid.matrix[fieldX][fieldY] = this;
 
-    this.moveDown = function () {
+    this.moveDown = function() {
         //check if block can move
-        if (this.fieldPosition.y + 1 >= fieldSize.y || fieldMatrix[this.fieldPosition.x][this.fieldPosition.y + 1] === 1) {
+        if (this.fieldPosition.y + 1 >= grid.size.y ||
+            grid.matrix[this.fieldPosition.x][this.fieldPosition.y + 1]) {
             return;
         }
 
         var originalX = this.x;
         var originalY = this.y;
 
-        fieldMatrix[this.fieldPosition.x][this.fieldPosition.y] = 0;
-        fieldMatrix[this.fieldPosition.x][++this.fieldPosition.y] = 1;
+        grid.matrix[this.fieldPosition.x][this.fieldPosition.y] = undefined;
+        grid.matrix[this.fieldPosition.x][++this.fieldPosition.y] = this;
 
         this.y += blockSize;
 
@@ -140,9 +147,19 @@ function FieldBlock(color, fieldX, fieldY) {
         }
     }
 
-   
+
 }
+
 FieldBlock.prototype = Block;
+
+var blockParams = {
+    blocks: 0,
+    rotationPivot: { x: 0, y: 0 }
+}
+var quadratic = {
+    tets : 0
+};
+quadratic.prototype = blockParams;
 
 function start() {
 
@@ -155,10 +172,57 @@ var test_spawnBlockEvery = 100;
 var test_elapsedTimeForSpawn = 0;
 var test_x = 0;
 var test_xIncrementMultiplier = 1;
+
+var clearRowActivated = false;
+var clearRows = [];
+var timeToClearRow = automaticMoveDownTimespan;
+var elapsedTimeForClearRow = 0;
+
 function update(deltaTime) {
     if (isGameOver) {
         return;
     }
+
+    //should clear some rows?
+    if (clearRowActivated) {
+        elapsedTimeForClearRow += deltaTime;
+        if (elapsedTimeForClearRow >= timeToClearRow) {
+            while (clearRows.length > 0) {
+                var y = clearRows.pop();
+                for (var x = 0; x < grid.size.x; ++x) {
+                    grid.matrix[x][y] = undefined;
+                }
+                var i = blocksOnField.length;
+                while (i--) {
+                    if (blocksOnField[i] && blocksOnField[i].fieldPosition.y === y) {
+                        blocksOnField[i].destroy();
+                        blocksOnField[i] = undefined;
+                        blocksOnField.splice(i, 1);
+                    }
+                }
+            }
+            clearRowActivated = false;
+            elapsedTimeForClearRow = 0;
+        }
+    }
+    //check if there is a full row and destroy it!
+    for (var y = 0; y < grid.size.y; ++y) {
+        var count = 0;
+        //count === x -> we can stop counting because there is a hole!
+        for (var x = 0; x < grid.size.x && count === x; ++x) {
+            count += grid.matrix[x][y] ? 1 : 0;
+        }
+        //did we find a full row?
+        if (count === grid.size.x) {
+            if ($.inArray(y, clearRows) === -1) {
+                //clear this row
+                clearRows.push(y);
+            }
+            clearRowActivated = true;
+        }
+    }
+
+    //automatic move down
     elapsedTimeForAutomaticMoveDown += deltaTime;
     if (elapsedTimeForAutomaticMoveDown >= automaticMoveDownTimespan) {
         blocksOnField.forEach(function(item) {
@@ -167,37 +231,15 @@ function update(deltaTime) {
         elapsedTimeForAutomaticMoveDown = 0;
     }
 
+    //automatic spawn for testing purposes
     test_elapsedTimeForSpawn += deltaTime;
     if (test_elapsedTimeForSpawn >= test_spawnBlockEvery) {
         blocksOnField.push(new FieldBlock("red", test_x, 0));
-        if (test_x + 1 * test_xIncrementMultiplier >= fieldSize.x || test_x + 1 * test_xIncrementMultiplier < 0) {
+        if (test_x + 1 * test_xIncrementMultiplier >= grid.size.x || test_x + 1 * test_xIncrementMultiplier < 0) {
             test_xIncrementMultiplier *= -1;
         }
         test_x += test_xIncrementMultiplier * 1;
         test_elapsedTimeForSpawn = 0;
-    }
-
-    //check if there is a full row and destroy it!
-    for (var y = 0; y < fieldSize.y; ++y) {
-        var count = 0;
-        //count === x -> we can stop counting because there is a hole!
-        for (var x = 0; x < fieldSize.x && count === x; ++x) {
-            count += fieldMatrix[x][y];
-        }
-        //did we find a full row?
-        if (count === fieldSize.x) {
-            for (var x = 0; x < fieldSize.x; ++x) {
-                fieldMatrix[x][y] = 0;
-            }
-            var i = blocksOnField.length;
-            while (i--) {
-                if (blocksOnField[i] && blocksOnField[i].fieldPosition.y === y) {
-                    blocksOnField[i].destroy();
-                    blocksOnField[i] = undefined;
-                    blocksOnField.splice(i, 1);
-                } 
-            }
-        }
     }
 
     //key mapping - the held shit doesn't work -> our update method deltatime is too fast -> consider other method!
@@ -213,8 +255,7 @@ function update(deltaTime) {
 }
 
 function render() {
-    var dirtyRectangleCount = dirtyRectangles.length;
-    for (var i = 0; i < dirtyRectangleCount; i += 1) {
+    while (dirtyRectangles.length > 0) {
         var rect = dirtyRectangles.pop();
 
         // clear this rectangle from the canvas
@@ -222,7 +263,7 @@ function render() {
     }
 
     //render dirty blocks delete
-    blocksOnField.forEach(function (block) {
+    blocksOnField.forEach(function(block) {
         if (block.dirty) {
             block.draw(fieldCanvasContext);
         }
